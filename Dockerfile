25d43b92711c7c7495dc7c504957dea2d4543f2c
Dave <dave5731@yahoo.com>
2023-08-08 12:24:12 -0400

    all files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM mcr.microsoft.com/dotnet/sdk:7.0 as build-env
+
+#RUN dotnet publish ./ci_cd_exp.csproj -c Release -o out --no-self-contained
+#RUN dotnet publish  "C:\\Users\\Dave\\Desktop\\code\\ci_cd_exp\\ci_cd_exp.csproj" 
+
+# Relayer the .NET SDK, anew with the build output
+FROM mcr.microsoft.com/dotnet/sdk:7.0
+COPY ./bin/Release/net7.0/ ./
+ENTRYPOINT [ "dotnet", "ci_cd_exp.dll" ]
---
+++ demo.cs
@@ -0,0 +1,13 @@
+/*
+namespace DemoApp
+{
+    class Demo {         
+        static void Main(string[] args)
+        {
+            String version ="1.0";
+            System.Console.WriteLine("****************** Demo App ******************");
+            System.Console.WriteLine("Version: "+version);
+            System.Console.WriteLine("Current Time: "+DateTime.Now.ToString("hh:mm:ss tt"));
+        }
+    }
+}*/
